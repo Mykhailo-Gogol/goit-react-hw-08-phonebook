465867b60ba242334b37c82e9e9e1804812bf920
mykhailo-gogol <mishhagogol@gmail.com>
2021-09-23 22:27:22 +0300

    adds bg image

--- src/AppStyle.ts
+++ src/AppStyle.ts
@@ -1,10 +1,12 @@
 import React from "react";
+import image from "./images/wall.jpeg";
 
 export const container = {
+  minHeight: "100vh",
   textAlign: "center",
   margin: "0 auto",
   padding: "0 20px",
-
+  backgroundImage: `url(${image})`,
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
